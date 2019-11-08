2e3f87a8117217867fe288aaeb9979bd4414319f
yzy <macy0122@126.com>
2019-11-08 10:32:20 +0800

    update

--- config-client1/src/main/java/com/yzy/configclient1/ConfigClient.java
+++ config-client1/src/main/java/com/yzy/configclient1/ConfigClient.java
@@ -2,19 +2,18 @@ package com.yzy.configclient1;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+@RestController
+@RefreshScope
 public class ConfigClient {
 	@Value("${from}")
 	private String from;
 
+	@GetMapping("fm")
 	public String getFrom() {
-		return from;
-	}
-
-	public void setFrom(String from) {
-		this.from = from;
+		return this.from;
 	}
 }
